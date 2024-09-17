970e637bfdca0e3821eaa2bafa56b2066442d01e
Marllon Home <m.w.p.ferreira@outlook.com>
2024-09-17 20:53:00 -0300

    add: buttons editar e excluir

--- adicionar.js
+++ adicionar.js
@@ -1,6 +1,6 @@
 const btnAdicionar = document.getElementById("form-container");
 
-btnAdicionar.addEventListener("submit",(event) => {
+btnAdicionar.addEventListener("submit",async (event) => {
     event.preventDefault()
 
 
@@ -14,20 +14,46 @@ btnAdicionar.addEventListener("submit",(event) => {
 
     const dto = {id, produto, description, price_buy, price_seller, dataDeValidade, localizacao}
     console.log(dto)
-    adiconarProduto(dto)
+    const result = await adicionarProduto(dto);
+    console.log(result)
+
+    if(result.success == true) {
+        Swal.fire({
+            title: "Muito bem!",
+            text: "O produto foi registrado!",
+            icon: "success"
+          });
+          document.getElementById("identificador").value = "";
+          document.getElementById("produto").value = "";
+          document.getElementById("descricao").value = "";
+          document.getElementById("precoDeCompra").value = "";
+          document.getElementById("dataValidade").value = "";
+          document.getElementById("localizacao").value = "";
+    }
+    if(result.success == false) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: result.message,
+          });
+    }
 });
 
-function adiconarProduto(dto) {
-    fetch('http://localhost:8000/istoky', {
-        method:"POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(dto)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
-    .catch(error => console.error('Erro:', error));
+async function adicionarProduto(dto) {
+    try {
+        const response = await fetch('http://localhost:8000/istoky', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dto)
+        });
+
+        const data = await response.json();
+        const { result } = data;
+
+        return result;
+    } catch (error) {
+        console.error('Erro:', error);
+    }
 }
--- script.js
+++ script.js
@@ -22,7 +22,19 @@ function adicionarProdutos(){
     const table =  document.getElementById("lista-produtos").getElementsByTagName('tbody')[0]
     console.log(listaProdutos)
 
+    
     listaProdutos.forEach(produto => {
+        const buttonDeletar =  document.createElement("button");
+        buttonDeletar.classList.add("button")
+        buttonDeletar.textContent = "Deletar";
+
+        const buttonEditar =  document.createElement("button");
+        buttonEditar.classList.add("button");
+        buttonEditar.textContent = "Editar"
+
+        buttonDeletar.addEventListener("click", () => deleteProduto(produto.id));
+        buttonEditar.addEventListener("click", () => abreForm());
+
         const newLine = table.insertRow();
 
         const celulaProduto = newLine.insertCell(0)
@@ -33,16 +45,61 @@ function adicionarProdutos(){
         const celulaPreçoDeVenda = newLine.insertCell(5)
         const celulaDataEntrada = newLine.insertCell(6)
         const celulaDataSaida = newLine.insertCell(7)
+        const celulaAcoes = newLine.insertCell(8)
 
-        celulaProduto.textContent = produto.produto
-        celulaDescricao.textContent = produto.description
-        celulaDataDeValidade.textContent = transformaDataString(produto.datadevalidade)
-        celulaLocalizacao.textContent = produto.localizacao
-        celulaPreçoDeCompra.textContent = produto.price_buy
-        celulaPreçoDeVenda.textContent = produto.price_seller
-        celulaDataEntrada.textContent = transformaDataString(produto.date_created)
-        celulaDataSaida.textContent = transformaDataString(produto.date_retired)
+        celulaAcoes.appendChild(buttonDeletar);
+        celulaAcoes.appendChild(buttonEditar);
+
+        celulaProduto.textContent = produto.produto;
+        celulaDescricao.textContent = produto.description;
+        celulaDataDeValidade.textContent = transformaDataString(produto.datadevalidade);
+        celulaLocalizacao.textContent = produto.localizacao;
+        celulaPreçoDeCompra.textContent = produto.price_buy;
+        celulaPreçoDeVenda.textContent = produto.price_seller;
+        celulaDataEntrada.textContent = transformaDataString(produto.date_created);
+        celulaDataSaida.textContent = transformaDataString(produto.date_retired);
     })
+}
+
+function deleteProduto(id) {
+    console.log(id)
+}
+
+async function abreForm() {
+    await Swal.fire({
+        title: "Multiple inputs",
+        html: `
+          <input id="swal-input1" class="swal2-input">
+          <input id="swal-input2" class="swal2-input">
+        `,
+        focusConfirm: false,
+        preConfirm: () => {
+          return [
+            document.getElementById("swal-input1").value,
+            document.getElementById("swal-input2").value
+          ];
+        }
+      });
+      if (formValues) {
+        Swal.fire(JSON.stringify(formValues));
+      }
+}
+
+async function editaProduto(dto) {
+    try {
+        const reponse = await fetch("http://localhost:8000/istoky", {
+            method: "PUT",
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(dto)
+        });
+        const data =  await reponse.json();
+        const { result } = data;
+        return result
+
+    } catch(erro) {
+    }
 }
 
 function transformaDataString(date) {
